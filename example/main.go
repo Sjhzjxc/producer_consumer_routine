d0872514a8c720c66328622377ed103ca9ce26d3
sjhzjxc <sjhzjxc@gmail.com>
2022-11-16 23:48:22 +0800

    1.以数组传递生产者消费者函数，支持多个生产者消费者线性运行 2.可重复使用

--- example/main.go
+++ example/main.go
@@ -8,8 +8,19 @@ import (
 
 func producer(executor func(interface{}) bool) {
 LOOP:
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10; i++ {
 		isBreak := executor(i)
+		if isBreak {
+			break LOOP
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func producer2(executor func(interface{}) bool) {
+LOOP:
+	for i := 0; i < 10; i++ {
+		isBreak := executor(i + 100)
 		if isBreak {
 			break LOOP
 		}
@@ -21,8 +32,32 @@ func consumer(value interface{}) {
 	fmt.Println(value)
 }
 
+func consumer2(value interface{}) {
+	fmt.Println(value, value.(int)+1)
+}
+
 func main() {
-	pc := producer_consumer_routine.DefaultProducerConsumer(producer, consumer)
+	content := producer_consumer_routine.ProducerConsumerContent{
+		Producer:      producer,
+		ProducerCount: 1,
+		Consumer:      consumer,
+		ConsumerCount: 1,
+	}
+	content2 := producer_consumer_routine.ProducerConsumerContent{
+		Producer:      producer2,
+		ProducerCount: 1,
+		Consumer:      consumer2,
+		ConsumerCount: 1,
+	}
+	contents := []producer_consumer_routine.ProducerConsumerContent{
+		content,
+		content2,
+	}
+	pc := producer_consumer_routine.DefaultProducerConsumer(contents)
+	go pc.Routine()
+	time.Sleep(10 * time.Second)
+	pc.Cancel()
+	pc.Wg.Wait()
 	go pc.Routine()
 	time.Sleep(10 * time.Second)
 	pc.Cancel()
